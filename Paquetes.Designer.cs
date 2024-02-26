36e76033ad79435d04c5fafd5bed0287ecb823f6
El_Oros <oroslp5@gmail.com>
2024-02-25 20:16:43 -0800

    subir programa

---
+++ Paquetes.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Sistema_de_deciciones_de_Funeraria
+{
+    partial class Paquetes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Paquetes));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bt_Oro = new System.Windows.Forms.Button();
+            this.bt_Estandar = new System.Windows.Forms.Button();
+            this.bt_Economico = new System.Windows.Forms.Button();
+            this.bt_Diamante = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(125, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Funeraria Camacho";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(130, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Elejidos para tu paz eterna";
+            // 
+            // bt_Oro
+            // 
+            this.bt_Oro.BackColor = System.Drawing.Color.Transparent;
+            this.bt_Oro.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bt_Oro.BackgroundImage")));
+            this.bt_Oro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_Oro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Oro.Location = new System.Drawing.Point(358, 138);
+            this.bt_Oro.Name = "bt_Oro";
+            this.bt_Oro.Size = new System.Drawing.Size(135, 135);
+            this.bt_Oro.TabIndex = 6;
+            this.bt_Oro.Text = "Paquete oro";
+            this.bt_Oro.UseVisualStyleBackColor = false;
+            this.bt_Oro.Click += new System.EventHandler(this.bt_Oro_Click);
+            // 
+            // bt_Estandar
+            // 
+            this.bt_Estandar.BackColor = System.Drawing.Color.Transparent;
+            this.bt_Estandar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bt_Estandar.BackgroundImage")));
+            this.bt_Estandar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_Estandar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Estandar.Location = new System.Drawing.Point(128, 138);
+            this.bt_Estandar.Name = "bt_Estandar";
+            this.bt_Estandar.Size = new System.Drawing.Size(135, 135);
+            this.bt_Estandar.TabIndex = 10;
+            this.bt_Estandar.Text = "Estandar";
+            this.bt_Estandar.UseVisualStyleBackColor = false;
+            this.bt_Estandar.Click += new System.EventHandler(this.bt_Estandar_Click);
+            // 
+            // bt_Economico
+            // 
+            this.bt_Economico.BackColor = System.Drawing.Color.Transparent;
+            this.bt_Economico.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bt_Economico.BackgroundImage")));
+            this.bt_Economico.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_Economico.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Economico.Location = new System.Drawing.Point(128, 302);
+            this.bt_Economico.Name = "bt_Economico";
+            this.bt_Economico.Size = new System.Drawing.Size(135, 135);
+            this.bt_Economico.TabIndex = 12;
+            this.bt_Economico.Text = "Economico";
+            this.bt_Economico.UseVisualStyleBackColor = false;
+            this.bt_Economico.Click += new System.EventHandler(this.bt_Economico_Click);
+            // 
+            // bt_Diamante
+            // 
+            this.bt_Diamante.BackColor = System.Drawing.Color.Transparent;
+            this.bt_Diamante.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bt_Diamante.BackgroundImage")));
+            this.bt_Diamante.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_Diamante.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Diamante.Location = new System.Drawing.Point(358, 302);
+            this.bt_Diamante.Name = "bt_Diamante";
+            this.bt_Diamante.Size = new System.Drawing.Size(135, 135);
+            this.bt_Diamante.TabIndex = 11;
+            this.bt_Diamante.Text = "Diamante";
+            this.bt_Diamante.UseVisualStyleBackColor = false;
+            this.bt_Diamante.Click += new System.EventHandler(this.bt_Diamante_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(137, 460);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(334, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Presione << ENTER >> para continuar";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(110, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 18);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Paquetes Disponibles:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(605, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 51);
+            this.button1.TabIndex = 16;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Paquetes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(668, 513);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bt_Economico);
+            this.Controls.Add(this.bt_Diamante);
+            this.Controls.Add(this.bt_Estandar);
+            this.Controls.Add(this.bt_Oro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Paquetes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "P A Q U E T E S    D I S P O N I B L E S ";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Paquetes_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bt_Oro;
+        private System.Windows.Forms.Button bt_Estandar;
+        private System.Windows.Forms.Button bt_Economico;
+        private System.Windows.Forms.Button bt_Diamante;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Paquetes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_deciciones_de_Funeraria
+{
+    public partial class Paquetes : Form
+    {
+        public Paquetes()
+        {
+            InitializeComponent();
+        }
+
+
+        private void bt_Estandar_Click(object sender, EventArgs e)
+        {
+            Datos da = new Datos();
+            da.Show();
+            this.Close();
+        }
+
+        private void bt_Economico_Click(object sender, EventArgs e)
+        {
+            Datos da = new Datos();
+            da.Show();
+            this.Close();
+        }
+
+        private void bt_Oro_Click(object sender, EventArgs e)
+        {
+            Datos da = new Datos();
+            da.Show();
+            this.Close();
+        }
+
+        private void bt_Diamante_Click(object sender, EventArgs e)
+        {
+            Datos da = new Datos();
+            this.Close();
+            da.Show();
+        }
+
+        private void Paquetes_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                Datos da = new Datos();
+                da.Show();
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
